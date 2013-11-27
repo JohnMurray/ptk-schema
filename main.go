4ec8f399599b925e33ca8b10980b622b5e8da76e
John Murray <me@johnmurray.io>
2013-11-27 16:56:28 -0500

    main.go and Makefile
    The make file is really just empty right now and the main.go is just the beginnings
    of something almost, mildly useful. More to come... obviously (hopefully).

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	/*
+	 * Parse incomign flags, dispatch to appropriate command or show help
+	 * message.
+	 */
+	flag.Parse()
+	var args []string = flag.Args()
+
+	if len(args) == 0 {
+		println("No command was given\n")
+		helpCommand()
+	} else {
+		commands := getCommands()
+
+		if cmd, ok := commands[args[0]]; ok {
+            cmd()
+        } else {
+            helpCommand()
+        }
+	}
+}
+
+/**
+ * Return a map of command-names (from user-input) to functions to
+ * run.
+ */
+func getCommands() map[string]func() {
+	return map[string]func(){
+		"help": helpCommand,
+	}
+}
+
+
+/**
+ * Print help information to the user... Straight forward enough taht I'll
+ * just stop typing about it now.
+ */
+func helpCommand() {
+	println("help command")
+}

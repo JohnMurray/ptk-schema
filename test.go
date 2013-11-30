18d4f08f39f4c0e90e4e8ba0122bcd887061d66a
John Murray <me@johnmurray.io>
2013-11-30 06:14:13 -0500

    test.go, config.json
    + Added config file to start using/testing with
    + Added test.go as a file just to play around with (currently w/ json parsing)

---
+++ test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type JsonConfig struct {
+	CommentToken string
+}
+
+func main() {
+	file, e := ioutil.ReadFile("./config.json")
+	if e != nil {
+		fmt.Printf("File Error: %v\n", e)
+		os.Exit(1)
+	}
+
+	// json_string := string(file)
+	// fmt.Println(json_string)
+
+	var config JsonConfig
+	err := json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Printf("Parse Error: %v\n", err)
+	}
+
+	fmt.Printf("%+v\n", config)
+    println(config.CommentToken)
+}

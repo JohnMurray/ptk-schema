b889a5818fdf2397b09cb80421ee052667ffdeb1
John Murray <me@johnmurray.io>
2013-12-01 11:44:26 -0500

    conifg.go - lookup user home directory
    Resolve the current user's home directory and lookup the config file (if
    one exists) for evaluation with the other config paths/files.

--- config.go
+++ config.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"os/user"
 )
 
 type Config struct {
@@ -32,14 +34,9 @@ type Config struct {
  * Returns a Config struct
  */
 func GetConfig() *Config {
-	var configLocations = []string{
-		"/etc/schema.conf",
-		"/usr/local/etc/schema.conf",
-		"$HOME/.schema.conf",
-		"./.schema.conf",
-	}
-
 	var config = new(Config)
+
+    var configLocations = getConfigPaths()
 
 	for i := range configLocations {
 		config = conflateConfigs(config,
@@ -50,13 +47,39 @@ func GetConfig() *Config {
 }
 
 /**
+ * Return all the paths that we will try to load configs from. The paths
+ * returned will be in priority order (first = lowest, last = highest)
+ */
+func getConfigPaths() []string {
+	var userConf string
+	usr, err := user.Current()
+	if err == nil {
+		userConf = usr.HomeDir + string(os.PathSeparator) + ".schema.conf"
+	}
+	return []string{
+		"/etc/schema.conf",
+		"/usr/local/etc/schema.conf",
+		userConf,
+		"./.schema.conf",
+	}
+}
+
+/**
  * Givne two configs, take care of merging them where appropriate. Any fields
  * that are specified within the override that are nil will be disregarded in
  * terms of override. This means that nil cannot be treated as a useful value
  * by the application.
  */
 func conflateConfigs(original *Config, overrides *Config) *Config {
-	return overrides
+	newConf := new(Config)
+
+	if overrides.CommentToken != "" {
+		newConf.CommentToken = overrides.CommentToken
+	} else {
+		newConf.CommentToken = original.CommentToken
+	}
+
+	return newConf
 }
 
 /**
--- test.go
+++ test.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
+    "fmt"
+    "os"
+	"os/user"
 )
 
-type JsonConfig struct {
-	CommentToken string
-}
-
 func main() {
-	file, e := ioutil.ReadFile("./config.json")
-	if e != nil {
-		fmt.Printf("File Error: %v\n", e)
-		os.Exit(1)
-	}
-
-	// json_string := string(file)
-	// fmt.Println(json_string)
-
-	config := new(JsonConfig)
-	err := json.Unmarshal(file, &config)
+	usr, err := user.Current()
 
 	if err != nil {
-		fmt.Printf("Parse Error: %v\n", err)
+        fmt.Printf("%+v", err)
+        os.Exit(1)
 	}
 
-	fmt.Printf("%+v\n", config)
-    println(config.CommentToken)
+    println(usr.HomeDir)
 }

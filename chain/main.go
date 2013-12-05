db1692e976a09647daaf839c49dc6612934e67aa
John Murray <me@johnmurray.io>
2013-12-05 10:40:17 -0500

    chain - placeholders
    Just added some placeholders for some of the alter-chain utils (functions
    types, and structs). Will implement them later, just trying to see what
    my program will/should look like before I go implementing everything.

--- chain/main.go
+++ chain/main.go
@@ -1,9 +1,35 @@
 package chain
 
+type Direction int
+
+const (
+	up   Direction = iota
+	down Direction = iota
+)
+
 type Chain struct {
 	ref      string
 	backref  *Chain
 	hasDown  bool
 	downFile string
 	upFile   string
+}
+
+type Meta struct {
+	ref       string
+	backref   string
+	direction Direction
+	filename  string
+}
+
+func fileList() /*[]string*/ {
+
+}
+
+func cwdIsSchemaDir() /*bool*/ {
+
+}
+
+func parseMeta(filepath string) /* *Meta*/ {
+
 }

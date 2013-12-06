e8901d132d168654575a8ec20f347983870a7ed6
John Murray <me@johnmurray.io>
2013-12-06 10:23:12 -0500

    Chain - Utility Functions
    Just commenting and doing a little bit of implementing
    for the utiilty functions that related to the alter-chain.

--- chain/main.go
+++ chain/main.go
@@ -1,11 +1,23 @@
 package chain
 
+import (
+	"errors"
+	"os"
+)
+
 type Direction int
 
 const (
 	up   Direction = iota
 	down Direction = iota
 )
+
+var (
+	ErrNotSchemaDir = errors.New(
+		"user: current directory is not a valid schema directory")
+)
+
+const metaFileName = ".schema.meta"
 
 type Chain struct {
 	ref      string
@@ -22,14 +34,37 @@ type Meta struct {
 	filename  string
 }
 
-func fileList() /*[]string*/ {
-
+/**
+ * For the current working directory, return a list of files that meet
+ * the criteria to be an alter (fit the naming convention) if the CWD
+ * is a schema directory. If the current directory is not a schema dir,
+ * then return and error
+ */
+func fileList() ([]string, error) {
+	if cwdIsSchemaDir() {
+		return nil, ErrNotSchemaDir
+	}
+	return []string{}, nil
 }
 
-func cwdIsSchemaDir() /*bool*/ {
+/**
+ * Check whether the current working directory is a schema directory. This
+ * can be checked by looking for the .schema.meta file within the CWD.
+ */
+func cwdIsSchemaDir() bool {
+	if currDir, err := os.Getwd(); err == nil {
+		metaFile := currDir + string(os.PathSeparator) + metaFileName
 
+		_, err = os.Stat(metaFile)
+        return err == nil
+	}
+	return false
 }
 
+/**
+ * Given a (valid) path to a file, read through the file and return a Meta
+ * object. If no file is returned, then a nil Meta object may be returned.
+ */
 func parseMeta(filepath string) /* *Meta*/ {
 
 }

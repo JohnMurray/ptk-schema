a18d51d3a21e730463cde9a434ded9e1709a42de
John Murray <me@johnmurray.io>
2013-12-27 11:43:31 -0500

    Chain Testing
    Added setup and teardown methods to go work with the test-env

--- utils/chain/chain_test.go
+++ utils/chain/chain_test.go
@@ -1,9 +1,36 @@
 package chain
 
 import (
+	"os"
 	"testing"
 )
 
+//
+// HELPER FUNCTIONS
+//
+
+func testSetup(t *testing.T) {
+	// validate in project directory running tests
+	if _, err := os.Stat("chain_test.go"); os.IsNotExist(err) {
+		t.Error("chain_test.go was not found. Make sure " +
+			"to run tests within the root project directory")
+	}
+
+	// change current working directory to the test-dir
+	if err := os.Chdir("./../../etc/test-env"); err != nil {
+		t.Fatal("Could not find test-directory './etc/test-env'")
+	}
+}
+
+func testTeardown(t *testing.T) {
+	// go back up to top-level directory
+	if err := os.Chdir("./../../utils/chain/"); err != nil {
+		t.Fatal("Could not go _back_ to root direcotry from test " +
+			"directory './etc/test-env'")
+	}
+}
+
 func TestDoNothing(t *testing.T) {
-	t.Fail()
+	testSetup(t)
+	testTeardown(t)
 }
